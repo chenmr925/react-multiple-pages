b44fae8a4906182a9ad2624bf9588776570ac791
chen <chenmr925@126.com>
2017-10-23 16:11:42 +0800

    修改项目结构

--- src/login/App.js
+++ src/login/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import '../css/App.scss';
-import yaoqing from '../img/yaoqing.png';
+import './App.scss';
+import logo from '../../public/logo.jpg';
 
 class App extends Component {
     render() {
         return (
             <div className="App-wrap">
-                <img src={yaoqing} className="App-logo" />
+                <img src={logo} className="App-logo" />
                 <div className="icon"></div>
                 <p className="App-logo">this is login page!!</p>
             </div>
